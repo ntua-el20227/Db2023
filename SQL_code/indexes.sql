2611a16f7cf5fc96699cab3de4470b19508aaefa
koustenischris <74160748+koustenischris@users.noreply.github.com>
2023-06-04 17:03:44 +0300

    Update indexes.sql

--- SQL_code/indexes.sql
+++ SQL_code/indexes.sql
@@ -1,14 +1,14 @@
 CREATE INDEX idx_user_school ON user(school_name);/*FK*/
+CREATE INDEX idx_user_username ON user(username);
+CREATE INDEX idx_user_role_name ON user(role_name);
 
 CREATE INDEX idx_applications_ISBN ON applications(ISBN);/*FK*/
 CREATE INDEX idx_applications_user ON applications(user_id);/*FK*/
+CREATE INDEX idx_applications_status_ ON applications(status_);
 
 CREATE INDEX idx_school_principal_last_name ON school(principal_last_name);
-CREATE INDEX idx_school_school_name ON school(school_name );
+CREATE INDEX idx_school_school_name ON school(school_name);
 
 CREATE INDEX idx_book_title ON book(title);
-CREATE INDEX idx_book_language ON book(language_);
 
-CREATE INDEX idx_stores_available_copies ON stores (available_copies);
-
-CREATE INDEX idx_category_name ON category(category_name);
+CREATE INDEX idx_review_like_scale ON review(like_scale);
